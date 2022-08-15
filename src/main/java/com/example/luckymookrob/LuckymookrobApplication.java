90dd684481afcf8be6a14443970cdfc2e1985cf9
spideypepo <petipol.nilpao@allianz.com>
2022-08-15 17:25:45 +0700

    first commit

---
+++ src/main/java/com/example/luckymookrob/LuckymookrobApplication.java
@@ -0,0 +1,13 @@
+package com.example.luckymookrob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuckymookrobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuckymookrobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/luckymookrob/LuckymookrobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.luckymookrob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuckymookrobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
